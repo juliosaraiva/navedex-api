b6cbdc5c8fc7d4bf2b455b0136d7ee33e440a97d
Julio Saraiva <contato@juliosaraiva.com.br>
2020-08-15 05:20:34 -0300

    chore: Added Postgres as Database and docker-compose file

--- src/navedex/settings.py
+++ src/navedex/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'navedex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD')
     }
 }
 
